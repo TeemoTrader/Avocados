7b9965df9b22b625ce99e2aab2846ec36621ac44
TeemoTrader <austinnorman1995@gmail.com>
2021-07-20 13:12:43 -0400

    Initial Commit

---
+++ Avocados/AvocadosApp.swift
@@ -0,0 +1,17 @@
+//
+//  AvocadosApp.swift
+//  Avocados
+//
+//  Created by Teemo Norman on 7/20/21.
+//
+
+import SwiftUI
+
+@main
+struct AvocadosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
